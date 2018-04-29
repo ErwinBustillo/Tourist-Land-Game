cc047f8a6bb2a63b9a56834bce7f841f2e1d43b5
erwinb1996@gmail.com <erwinb1996@gmail.com>
2018-04-29 10:57:02 -0500

    update achievment,npc scripts

--- Assets/Scripts/AchievementManager.cs
+++ Assets/Scripts/AchievementManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class AchievementManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+	public string key1="";
+	public string key2="";
+	public string key3="";
+	public string key4="";
+	public string key5="";
+
+
+
+	// limpia todas las llaves
+	public void ResetAllProgress(){
+		PlayerPrefs.DeleteAll ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// guarda el estado del logro 
+	public void SaveKey(string key,int value){
+		if (!PlayerPrefs.HasKey(key)) {
+			PlayerPrefs.SetInt (key, value);
+			PlayerPrefs.Save ();
+		}
 	}
 }
--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Npc : MonoBehaviour {
 
+	public enum STATES
+	{
+		idle, 
+		walking,
+	}
+
+	public STATES defaultState;
+	public Collider visorCollider;
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			defaultState = STATES.idle;
+			transform.LookAt (other.gameObject.transform);
+		}
+	}
+
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.tag == "Player") {
+			defaultState = STATES.walking;
+		}
+	}
+
 }
