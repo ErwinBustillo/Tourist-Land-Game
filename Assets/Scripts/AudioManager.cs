4513b4d5a959005ecede76f5db457b57b31cb6a7
erwinb1996@gmail.com <erwinb1996@gmail.com>
2018-05-14 18:30:01 -0500

    added audiomanager with soundtracks for each scene, and more features

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof (AudioSource))]
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip[] soundtracks; 
+	private AudioSource audioSource;
+	int index = 0;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = soundtracks [index];
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audioSource.isPlaying) {
+			index = Random.Range (0, soundtracks.Length) % soundtracks.Length;
+			audioSource.clip = soundtracks [index];
+			audioSource.Play ();
+		}
+	}
+}
